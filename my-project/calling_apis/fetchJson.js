f0feb5ef515bbd44b19e00dd0ed13e529d7205cf
delphine <delphine.mubiligi@gmail.com>
2022-07-28 12:20:57 +0100

    moved folder to new repo

--- my-project/calling_apis/fetchJson.js
+++
@@ -1,3 +0,0 @@
-const fetchJson = (url) => {
-    
-}
--- my-project/testing/multiply.test.js
+++
@@ -1,7 +0,0 @@
-const multiply = require("./multiply");
-
-describe('multiply', () => {
-    it('multiplies 5 and 5', () => {
-        expect(multiply(5, 5)).toBe(25);
-    });
-});
